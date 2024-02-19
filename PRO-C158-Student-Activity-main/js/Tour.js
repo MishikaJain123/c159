466a8dab779348f23bdb87b8dfcf88c2dcafed96
MishikaJain123 <155298829+MishikaJain123@users.noreply.github.com>
2024-02-19 16:58:43 +0530

    Add files via upload

---
+++ PRO-C158-Student-Activity-main/js/Tour.js
@@ -0,0 +1,100 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards();
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "BatMan",
+        title: "BatMan",
+        url: "assets/thumbnails/download.jpeg",
+      },
+      {
+        id: "WonderWoman",
+        title: "WonderWoman",
+        url: "assets/thumbnails/wonder-woman-nc2b05-cover-r.a.-and-d.g.-1985-federal-comics-australia.png",
+      },
+
+      {
+        id: "Hulk",
+        title: "Hulk",
+        url: "assets/thumbnails/images.jpeg",
+      },
+      {
+        id: "SpiderMan",
+        title: "SpiderMan",
+        url: "assets/0_222f.jpg",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width:22,
+      height:30
+    });
+    entityEl.setAttribute("position", position);
+    
+
+    //Add cursor-listener component to the ring border entity to change it's color 
+    //On Cursor 'mouseenter' and 'mouseleave' entity
+    entityEl.setAttribute("cursor-listener", {});
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width:20,
+      height:28
+    });
+    entityEl.setAttribute("material", { src: item.url });
+    entityEl.setAttribute("position", { x:0 , y:0.5 , z:0.1 });
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    
+    return entityEl;
+  },
+});
